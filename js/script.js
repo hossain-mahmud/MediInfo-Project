bcb7ea69e7b9fff642192b468d2a202f6f0015d5
hossain <hossainmahmud.cse@gmail.com>
2023-10-25 01:03:28 +0600

    footer part add

--- js/script.js
+++ js/script.js
@@ -6,39 +6,54 @@ $('.review_count').counterUp({
 //----- banner review number counter end
 // ===== app screenshot slider part start
 $('.screenshot_slider').slick({
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1500,
-  prevArrow:false,
-  nextArrow:false,
-  dots:true,
-  centerMode:false,
-     responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    prevArrow: false,
+    nextArrow: false,
+    dots: true,
+    centerMode: false,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
-  // ----- app screenshot slider part end
-                
+// ----- app screenshot slider part end
+// ===== counter slider part start
+$('.count').counterUp({
+    delay: 10,
+    time: 2000
+});
+// ----- counter slider part end
+// ===== testimonial part start
+$('.testimonial_slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    nextArrow: '<i class="fa-solid next_arrrow fa-chevron-right"></i>',
+    prevArrow: '<i class="fa-solid pree_arrow fa-chevron-left"></i>',
+    dots: true,
+});
+// ----- testimonial slider part end   
