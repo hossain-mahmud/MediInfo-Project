761c4eb05bee5ae4fd89c1554286eaad2107180d
hossain <hossainmahmud.cse@gmail.com>
2023-10-24 23:04:15 +0600

    screenshot slider add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
-// banner review number counter start
+// ===== banner review number counter start
 $('.review_count').counterUp({
     delay: 10,
     time: 3000
 });
+//----- banner review number counter end
+// ===== app screenshot slider part start
+$('.screenshot_slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  prevArrow:false,
+  nextArrow:false,
+  dots:true,
+  centerMode:false,
+     responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+  // ----- app screenshot slider part end
+                
