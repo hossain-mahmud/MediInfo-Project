ef8960fa84a207b63d84fee90864cec6620c85c4
hossain <hossainmahmud.cse@gmail.com>
2023-10-29 22:01:10 +0600

    add back to top button add

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-// ===== nav sticky start
-let main_menu= document.querySelector('.main_menu');
-window.addEventListener('scroll',()=>{
+// ===== nav and top to back sticky start
+let main_menu = document.querySelector('.main_menu');
+let back_to_top = document.querySelector('.back_to_top');
+window.addEventListener('scroll', () => {
     let scrooling = this.scrollY;
-    if(scrooling >110){
+    if (scrooling > 110) {
         main_menu.classList.add('nav_sticky');
-    }
-    else{
+        back_to_top.classList.add('back_to_top_fixed')
+    } else {
         main_menu.classList.remove('nav_sticky');
+        back_to_top.classList.remove('back_to_top_fixed')
     }
 })
-//----- nav sticky end
+back_to_top.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
+})
+//----- nav and top to back sticky end
 // ===== banner review number counter start
 $('.review_count').counterUp({
     delay: 10,
